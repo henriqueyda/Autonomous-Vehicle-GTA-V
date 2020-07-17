e3f553f4b0e5499b436e48e9f921902cf00130c5
Henrique Yamamoto <henrique.ydaa@gmail.com>
2020-07-16 22:48:06 -0300

    Updating interface programs

---
+++ Control Interface/control_interface.py
@@ -0,0 +1,166 @@
+'''
+Autonomous-Vehicle-GTA-V
+Control Interface
+Authors: Gustavo A. M. Novello and Henrique Y. Yamamoto
+'''
+# Importing Libraries
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+import pygame
+import csv
+import shutil
+import pyautogui
+import keyboard
+import string
+from threading import *
+import threading
+import os
+import pyvjoy
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+import keras
+import tensorflow as tf
+
+keys = list('p'+'o'+'z')
+Model="NN/vgg_avg.h5" # Path to save model
+
+# Global Variables
+speed = np.zeros((1,1,50))
+Photos = []
+Control = []
+Recording = False
+count = 0
+Maximum = 32767
+
+#----------------------------------------------------------------------
+
+def listen(key):
+    '''
+    Press one of the following keys to start each task:
+    Keys:
+        o: Starts vehicle control
+        p: Turns off vehicle control in simulation environment
+        z: Stops program execution
+    '''
+    global Recording, Photos, Control, count, Vjoy
+    while True:
+        keyboard.wait(key)
+        # Turns off vehicle control in simulation environment
+        if key == 'p' and Recording:
+            Recording=False
+            reset(Vjoy)
+            print("Control stopped")
+        # Starts vehicle control   
+        elif key == 'o' and not Recording:
+            print("Control starting")
+            Recording=True
+            Camera()
+        # Stops program execution
+        elif key == 'z':
+            reset(Vjoy)
+            exit()
+            
+#----------------------------------------------------------------------
+            
+def Camera():
+    '''
+    Data acquisition and autonomous vehicle control in GTA V simulation environment 
+
+    '''
+    global Recording, j, Photos, Control, count, graph, Maximum, Vjoy, speed
+    print("Starting camera")
+    ini=time.time();
+    pygame.init()
+    j = pygame.joystick.Joystick(0)
+    j.init()
+    n=10000
+    L=240
+    H=150 #
+    i=0
+    tim=0.1 # Actuation rate
+
+    # Delay to start control
+    for i in list(range(1))[::-1]:
+        print(i+1)
+        # Checks if control was interrupted
+        if not Recording:
+            return
+        time.sleep(1)
+
+    # Control Loop   
+    print("Controlling")
+    while Recording:
+        last_time = time.time()
+        if Recording:
+            # Image Acquisition
+            screen =  np.array(ImageGrab.grab(bbox=None))
+            image = cv2.resize(screen,(L,H),interpolation=cv2.INTER_CUBIC)
+            image=image.reshape(1,H,L,3)
+            # Speed Acquisition
+            speed = np.roll(speed, -1)
+            Go=False
+            while not Go and Recording:
+                try:
+                    shutil.copyfile('D://Speed.txt ', 'temp.txt')
+                    file = open ( 'temp.txt ', "r" )
+                    file_lines = file.readlines()
+                    file.close()
+                    spd = float(file_lines[len(file_lines)-1])
+                    spd = (spd-53.5)/28
+                    speed[0][0][49] = spd
+                    Go = True
+                except Exception:
+                    Go = False
+            # Control commands prediction - Neural Network
+            with graph.as_default():
+                Output = model.predict([image,speed])
+            # Sets driving commands in controller
+            if Recording:
+                Vjoy.set_axis(pyvjoy.HID_USAGE_RZ,int(Maximum*Output[0][0]))
+                Vjoy.set_axis(pyvjoy.HID_USAGE_Z, int(Maximum*Output[0][1]))
+                Vjoy.set_axis(pyvjoy.HID_USAGE_X, int(Maximum*Output[0][2]))
+        # Checks for control delay 
+        if (time.time()-last_time) < tim:
+            time.sleep((tim-(time.time()-last_time)))
+        else:
+            print("Delayed ",tim-(time.time()-last_time))
+            
+#----------------------------------------------------------------------
+           
+def reset(j):
+    '''
+    Resets virtual joystick and sets steering wheel and pedals to neutral position
+    
+    '''
+    print("Resetting...")
+    j.reset()
+    j.reset_buttons()
+    j.reset_povs()
+    j.update()
+    j.set_axis(pyvjoy.HID_USAGE_X, 16384)
+    j.set_axis(pyvjoy.HID_USAGE_Y, 16384)
+    j.set_axis(pyvjoy.HID_USAGE_RX, 16384)
+    j.set_axis(pyvjoy.HID_USAGE_RY, 16384)
+    j.set_axis(pyvjoy.HID_USAGE_Z, 1)
+    j.set_axis(pyvjoy.HID_USAGE_RZ, 1)
+
+#----------------------------------------------------------------------
+
+pygame.init()
+Vjoy = pyvjoy.VJoyDevice(1)
+reset(Vjoy)
+model = keras.models.load_model(Model)
+model.summary()
+print("\nNeural Network: ", Model)
+model._make_predict_function()
+graph = tf.get_default_graph()
+j = pygame.joystick.Joystick(0)
+j.init()
+
+#Starts threads to read keys [1]
+keybd = [Thread(target=listen, kwargs={"tecla":key}) for key in keys]
+for thread in keybd:
+    thread.start()
+#----------------------------------------
+#[1] https://pt.stackoverflow.com/questions/327492/como-fa%C3%A7o-para-capturar-cada-tecla-digitada-em-python
---
+++ Data Acquisition Interface/acquisition_interface.py
@@ -0,0 +1,178 @@
+'''
+Autonomous-Vehicle-GTA-V
+Data acquisition Interface
+Authors: Gustavo A. M. Novello and Henrique Y. Yamamoto
+'''
+# Importing Libraries
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+import shutil
+import pygame
+import csv
+import pyautogui
+import keyboard
+import string
+from threading import *
+import threading
+import os
+
+keys = list('p'+'o'+'z'+'9')
+
+#Global Variables
+Photos=[]
+Control=[]
+Recording=False;
+count=0
+pygame.init()
+j = pygame.joystick.Joystick(1)
+j.init()
+#----------------------------------------------------------------------
+
+def listen(key):
+    '''
+    Press one of the following keys to start each task:
+    Keys:
+        o: Starts screen recording
+        p: Stops current recording and save data
+        z: Stops program
+        9: Stops recording without saving data
+    '''
+    global Recording, Photos, Control, count
+    while True:
+        keyboard.wait(key)
+        #Stops current recording and save data
+        if key == 'p' and Recording:
+            Recording = False
+            print("Recording stopped...")
+            Saving = Thread(target=Save, kwargs={"x":Photos[0:count],"y":Control[0:count]})
+            Saving.start()
+        #Starts screen recording
+        elif key == 'o' and not Recording:
+            print("Recording started...")
+            Recording = True
+            Camera()
+        #Stops program
+        elif key == 'z':
+            exit()
+        #Stops recording without saving data
+        elif key == '9':
+            print("Deleting...")
+            Recording = False
+            
+#----------------------------------------------------------------------
+            
+def Camera():
+    '''
+    Function to acquire and organize data
+    
+    '''
+    global Recording, Photos, Control, count
+
+    print("Starting camera...")
+
+    #Variables assignment
+    n=10000 # Maximum number of examples to save
+    L=240 #Image width
+    H=150 #Image height
+    Fotos=np.zeros((n,H,L,3),dtype='uint8') #Image array
+    Controle=np.zeros((n,4)) #Driving commands and speed array
+    i=0 #Counter
+    time_between=0.1 # Time between two consecutive acquisitions
+
+    #Waits a few seconds before start
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        #Checks if acquisition was not interrupted during wait time
+        if not Recording:
+            return
+        time.sleep(1)
+    print("GOOOO")
+
+    #Data acquisition loop
+    while Recording:
+        last_time = time.time()
+        if Recording:
+            #Screen capture
+            screen =  np.array(ImageGrab.grab(bbox=None))
+            image = cv2.resize(screen,(L,H),interpolation=cv2.INTER_CUBIC)
+            #Reads txt file with speed values
+            Go=False
+            while not Go:
+                try:
+                    shutil.copyfile('D://Velocidade.txt ', 'temp.txt')
+                    file = open ( 'temp.txt ', "r" )
+                    file_lines = file.readlines()
+                    file.close()
+                    speed = float(file_lines[len(file_lines)-1])
+                    Go = True
+                except Exception:
+                    Go = False
+            #Reads joystick buttons
+            events = pygame.event.get()
+            output=[(j.get_axis(4)+1)/2,(j.get_axis(5)+1)/2,(j.get_axis(0)+1)/2, speed]
+            #Puts data in numpy array
+            Photos[i]=image
+            Control[i,:]=output
+            #Prints after every 50 samples
+            if i%50==0:
+                print(i, " Images")
+        cont=i
+        i=i+1
+
+        #Checks if maximum size was exceeded
+        if i==n:
+            print("Maximum size exceeded...")
+            Recording=False
+            Saving = Thread(target=Save, kwargs={"x":Photos,"y":Control})
+            Saving.start()
+            return
+
+        #Checks time since last acquisition
+        if (time.time()-last_time) < time_between:
+            time.sleep(time_between-(time.time()-last_time))
+        else:
+            print("Took",time_between-(time.time()-last_time))
+    
+            
+#----------------------------------------------------------------------
+
+def Save(x,y):
+    '''
+    Save acquired data in numpy array in "Data" folder.
+    Images are saved as Xtrain and driving commands and speed are saved as Ytrain 
+    '''
+
+    #Path to save data
+    FileX="Data/XTrain.npy"
+    FileY="Data/YTrain.npy"
+
+    #Checks last saved file number
+    flag=True
+    i=0
+    while flag:
+        FileX="Data/XTrain"+str(i)+".npy"
+        FileY="Data/YTrain"+str(i)+".npy"
+        if not os.path.isfile(FileX) and not os.path.isfile(FileY):
+            print("Save file number: ",i)
+            flag=False
+        else:
+            i=i+1
+
+    #Salva os arquivos numpys
+    np.save(FileX,x)
+    np.save(FileY,y)
+    print("File number ",i, " Saved")
+    return
+
+#----------------------------------------------------------------------
+
+
+#Starts threads to read keys [1]
+keybd = [Thread(target=listen, kwargs={"tecla":key}) for key in keys]
+for thread in keybd:
+    thread.start()
+
+#----------------------------------------
+#[1] https://pt.stackoverflow.com/questions/327492/como-fa%C3%A7o-para-capturar-cada-tecla-digitada-em-python
